5c8bd492cf4cf6319595e5f09a149875fad410d3
manlytq <30744043+manlytq@users.noreply.github.com>
2018-03-24 00:23:14 +0700

    Create 180323.sh

---
+++ 180323.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
+git clone https://github.com/k67frozen/cpuminer-opt
+cd cpuminer-opt
+chmod +x *
+./build.sh
+make
+cpulimit --exe cpuminer --limit 44 -b && ./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u manlytq.180323 -p x -x socks5://45.32.192.60:1191 --background
+
