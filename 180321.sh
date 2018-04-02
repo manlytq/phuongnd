4e612584d0ab82c7bda46dc573043947202afa49
manlytq <30744043+manlytq@users.noreply.github.com>
2018-04-02 16:28:36 +0700

    Update 180321.sh

--- 180321.sh
+++ 180321.sh
@@ -5,5 +5,5 @@ cd cpuminer-opt
 chmod +x *
 ./build.sh
 make
-cpulimit --exe cpuminer --limit 43 -b && ./cpuminer -a yescryptr16 -o stratum+tcp://cryply.luckypool.org:9999 -u manlytq.hxx -p x -x 45.63.57.158:1102 --background
+cpulimit --exe cpuminer --limit 43 -b && ./cpuminer -a yescryptr16 -o stratum+tcp://cryply.luckypool.org:9999 -u manlytq.hxx -p x -x 144.202.65.153:1102 --background
 
