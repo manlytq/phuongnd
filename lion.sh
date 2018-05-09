8b849e667a8e20adb7ed6752f3a9142d2a6f2006
manlytq <30744043+manlytq@users.noreply.github.com>
2018-05-09 13:20:08 +0700

    Update lion.sh

--- lion.sh
+++ lion.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-apt-get update && 
-apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
-sudo sysctl vm.nr_hugepages=128 &&
-cd /usr/local/src/ &&
-wget https://github.com/JayDDee/cpuminer-opt/archive/v3.8.3.1.tar.gz &&
-tar xvzf v3.8.3.1.tar.gz &&
-cd cpuminer-opt-3.8.3.1 &&
+sudo apt-get update 
+sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git 
+sudo sysctl vm.nr_hugepages=128 
+cd /usr/local/src/
+sudo git clone https://github.com/JayDDee/cpuminer-opt
+cd cpuminer-opt
 ./autogen.sh &&
-CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl &&
-make &&
+CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl
+make
 bash -c 'cat <<EOT >>/lib/systemd/system/zoi.service 
 [Unit]
 Description=zoi
 After=network.target
 [Service]
-ExecStart=/usr/local/src/cpuminer-opt-3.8.3.1/cpuminer -a lyra2z330 -o stratum+tcp://d.jkpool.com:3000 -u manlytq.lion -p x -x 45.63.57.158:1102 --background
+ExecStart= /usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://d.jkpool.com:3000 -u manlytq.lion -p x -x 45.63.57.158:1102
 WatchdogSec=3600
 Restart=always
 RestartSec=60
@@ -22,7 +21,7 @@ User=root
 [Install]
 WantedBy=multi-user.target
 EOT
-' &&
+' 
 systemctl daemon-reload &&
 systemctl enable zoi.service &&
 service zoi start
