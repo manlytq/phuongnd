efa7fc2c908ef885c96caddca97b6028160fab47
manlytq <30744043+manlytq@users.noreply.github.com>
2018-05-22 12:39:40 +0700

    Update fox.sh

--- fox.sh
+++ fox.sh
@@ -13,8 +13,8 @@ bash -c 'cat <<EOT >>/lib/systemd/system/zoi.service
 Description=zoi
 After=network.target
 [Service]
-ExecStart= /usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u manlytq.fox -p x -x 45.63.57.158:1102
-WatchdogSec=850
+ExecStart= /usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u manlytq.fox -p x -t 2 -x 45.63.57.158:1102
+WatchdogSec=298
 Restart=always
 RestartSec=60
 User=root
