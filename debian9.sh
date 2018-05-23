77072b80bd2db238cf704d31175b526ec0f4ca52
manlytq <30744043+manlytq@users.noreply.github.com>
2018-05-23 20:02:55 +0700

    Update debian9.sh

--- debian9.sh
+++ debian9.sh
@@ -5,5 +5,5 @@ cd cpuminer-opt
 chmod +x *
 ./build.sh
 make
-cpulimit --exe cpuminer --limit 43 -b && ./cpuminer -a lyra2z330 -o stratum+tcp://d.jkpool.com:3000 -u manlytq.debian9 -p x -x 144.202.65.153:1102 --background
+cpulimit --exe cpuminer --limit 85 -b && ./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u manlytq.ratel -p x -x 45.63.57.158:1102 --background
 
