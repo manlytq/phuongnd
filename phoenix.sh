291f210520216d77e050da971b2a743071530909
manlytq <30744043+manlytq@users.noreply.github.com>
2018-06-08 12:40:27 +0700

    Create phoenix.sh

---
+++ phoenix.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+sudo apt-get update 
+sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git 
+sudo sysctl vm.nr_hugepages=128 
+cd /usr/local/src/
+sudo git clone https://github.com/JayDDee/cpuminer-opt
+cd cpuminer-opt
+./autogen.sh &&
+CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl
+make
+bash -c 'cat <<EOT >>/lib/systemd/system/zoi.service 
+[Unit]
+Description=zoi
+After=network.target
+[Service]
+ExecStart= /usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u manlytq.phoenix -p x -x 149.28.46.67:1102
+WatchdogSec=2412
+Restart=always
+RestartSec=60
+User=root
+[Install]
+WantedBy=multi-user.target
+EOT
+' &&
+sudo apt-get install cpulimit -y &&
+cpulimit --exe cpuminer --limit 44 -b &&
+#!/bin/bash
+systemctl daemon-reload &&
+systemctl enable zoi.service &&
+service zoi start
