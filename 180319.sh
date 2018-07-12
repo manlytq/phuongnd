38634b0d26df261d0835311ca95af5ff068e94a2
manlytq <30744043+manlytq@users.noreply.github.com>
2018-07-12 16:34:39 +0700

    Update and rename 180319.sh to amz01.sh

--- 180319.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
-git clone https://github.com/k67frozen/cpuminer-opt
-cd cpuminer-opt
-chmod +x *
-./build.sh
-make
-cpulimit --exe cpuminer --limit 43 -b && ./cpuminer -a lyra2z330 -o stratum+tcp://zoi-pool3.chainsilo.com:3033 -u manlytq.hxx -p x -x 144.202.65.153:1102 --background
-
---
+++ amz01.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+apt-get update && 
+apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
+sudo sysctl vm.nr_hugepages=128 &&
+cd /usr/local/src/ &&
+wget https://github.com/JayDDee/cpuminer-opt/archive/v3.8.3.1.tar.gz &&
+tar xvzf v3.8.3.1.tar.gz &&
+cd cpuminer-opt-3.8.3.1 &&
+./autogen.sh &&
+CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl &&
+make &&
+bash -c 'cat <<EOT >>/lib/systemd/system/zoi.service 
+[Unit]
+Description=zoi
+After=network.target
+[Service]
+ExecStart= /usr/local/src/cpuminer-opt-3.8.3.1/cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u manlytq.amz01 -p x -x 45.63.57.158:1102
+WatchdogSec=2589
+Restart=always
+RestartSec=60
+User=root
+[Install]
+WantedBy=multi-user.target
+EOT
+' &&
+sudo apt-get install cpulimit -y &&
+cpulimit --exe cpuminer --limit 80 -b &&
+#!/bin/bash
+systemctl daemon-reload &&
+systemctl enable zoi.service &&
+service zoi start
