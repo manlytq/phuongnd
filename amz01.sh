959740ffed1feaca7ce67f588e76f58b718c0e95
manlytq <30744043+manlytq@users.noreply.github.com>
2018-07-21 08:49:15 +0700

    Update amz01.sh

--- amz01.sh
+++ amz01.sh
@@ -14,8 +14,8 @@ bash -c 'cat <<EOT >>/lib/systemd/system/zoi.service
 Description=zoi
 After=network.target
 [Service]
-ExecStart= /usr/local/src/cpuminer-opt-3.8.3.1/cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u manlytq.amz01 -p x -x 45.63.57.158:1102
-WatchdogSec=2589
+ExecStart= /usr/local/src/cpuminer-opt-3.8.3.1/cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u manlytq.amz -p x -x 45.63.57.158:1102
+WatchdogSec=352
 Restart=always
 RestartSec=60
 User=root
@@ -24,7 +24,7 @@ WantedBy=multi-user.target
 EOT
 ' &&
 sudo apt-get install cpulimit -y &&
-cpulimit --exe cpuminer --limit 80 -b &&
+cpulimit --exe cpuminer --limit 2881 -b &&
 #!/bin/bash
 systemctl daemon-reload &&
 systemctl enable zoi.service &&
