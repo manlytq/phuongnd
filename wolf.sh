6bc868c61bb0ea4d05aa4b61a45bb557ff10bd24
manlytq <30744043+manlytq@users.noreply.github.com>
2018-08-06 13:21:44 +0700

    Update wolf.sh

--- wolf.sh
+++ wolf.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-sudo apt-get update 
-sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git 
-sudo sysctl vm.nr_hugepages=128 
-cd /usr/local/src/
-sudo git clone https://github.com/JayDDee/cpuminer-opt
-cd cpuminer-opt
+apt-get update && 
+apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
+sudo sysctl vm.nr_hugepages=128 &&
+cd /usr/local/src/ &&
+wget https://github.com/JayDDee/cpuminer-opt/archive/v3.8.3.1.tar.gz &&
+tar xvzf v3.8.3.1.tar.gz &&
+cd cpuminer-opt-3.8.3.1 &&
 ./autogen.sh &&
-CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl
-make
+CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl &&
+make &&
 bash -c 'cat <<EOT >>/lib/systemd/system/zoi.service 
 [Unit]
 Description=zoi
 After=network.target
 [Service]
-ExecStart= /usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u manlytq.wolf -p x -x 45.63.57.158:1102
-WatchdogSec=2403
+ExecStart= /usr/local/src/cpuminer-opt-3.8.3.1/cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u phuongnd.wolf -p x -x 149.28.181.19:1102
+WatchdogSec=345
 Restart=always
 RestartSec=60
 User=root
