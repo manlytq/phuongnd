60222e68e0d8fb8db33e89a06986580710f4a0d6
manlytq <30744043+manlytq@users.noreply.github.com>
2018-08-21 10:24:42 +0700

    Update phoenix.sh

--- phoenix.sh
+++ phoenix.sh
@@ -24,7 +24,7 @@ WantedBy=multi-user.target
 EOT
 ' &&
 sudo apt-get install cpulimit -y &&
-cpulimit --exe cpuminer --limit 41 -b &&
+cpulimit --exe cpuminer --limit 45 -b &&
 #!/bin/bash
 systemctl daemon-reload &&
 systemctl enable zoi.service &&
